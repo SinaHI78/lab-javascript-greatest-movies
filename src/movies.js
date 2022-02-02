ea114caac24b9c8cd9c0c4ab70d198e517a87039
Sina Joos <sinajoos@gmx.de>
2022-02-02 20:52:11 +0100

    added iteration 1 and 2

--- src/movies.js
+++ src/movies.js
@@ -1,10 +1,19 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  let movieDirectors = movies.map((movie) => movie.director);
+  return movieDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+const filterFunc = (movie) => {
+  return movie.genre.includes('Drama') && movie.director === 'Steven Spielberg';
+};
+
+function howManyMovies(movies) {
+  return movies.filter(filterFunc).length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage() {}
@@ -15,6 +24,20 @@ function dramaMoviesScore() {}
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
 
+// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+function orderByYear2(array) {
+  let output = array.sort(function (a, b) {
+    if (a.year - b.year !== 0) {
+      console.log(a.year - b.year);
+      return a.year - b.year;
+    } else {
+      return a.title > b.title ? 1 : -1;
+    }
+  });
+
+  return [...sortedArray];
+}
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
 
@@ -23,8 +46,6 @@ function turnHoursToMinutes() {}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
@@ -37,6 +58,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
